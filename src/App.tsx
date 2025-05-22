1046017e62e23c8b306af4e034f6d0c4d81b9fe8
Nievas2 <angelgabrielnievas@gmail.com>
2025-05-22 16:24:46 -0300

    refactor(App): :recycle: Remove commented Navbar import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
-import { Navbar } from "./components/shared/Navbar"
+import HomePage from "./pages/home/HomePage"/* 
+import { Navbar } from "./components/shared/Navbar" */
 
 export function App() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-800">
-      <Navbar />
+     {/*  <Navbar /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
